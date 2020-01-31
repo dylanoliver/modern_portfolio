793ddb6dbc0ac53ec2ee04072e7f4d4499f667e2
Dylan Oliver <dylanoliver@me.com>
2020-01-31 11:39:30 +0000

    Updates

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set initial state of menu, as there's more than one nav-item, we cannot use a constant
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        // As there;s more than 1, loop through the nav-items with a high order array
+        navItems.forEach(item => item.classList.add('show'));
+        // set menu state
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // set menu state
+        showMenu = false;
+    }
+}
